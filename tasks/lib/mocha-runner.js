913023851ccc4bd0469e7916f64ed8e077b6e515
jharwig <jason@pinepointsoftware.com>
2014-02-27 12:31:27 -0600

    Fix error

--- tasks/lib/mocha-runner.js
+++ tasks/lib/mocha-runner.js
@@ -43,7 +43,7 @@ module.exports = function(options, browser, grunt, wd, fileGroup){
          var filename = [
              testFile, 
              ((number + '').length === 1 ? '0' : '') + ('' + number),
-             this.currentTest.title.replace(/[^a-zA-Z0-9 ]+/g, '').replace(/(^\s*|\s*$)/g, '').replace(/\s+/g, '_').substring(0, 200)
+             this.currentTest.title.replace(/[^a-zA-Z0-9 ]+/g, '').replace(/(^\s*|\s*$)/g, '').replace(/\s+/g, '_').substring(0, 200),
              browserName
          ].join('-');
          return browser.saveScreenshot(options.screenshotDir + '/' + filename.substring(0,250) + ".png")
